973da298696a44dd098973856e0f3eb16ba233f7
dohan <100039401+doan312@users.noreply.github.com>
2024-06-01 22:03:49 +0900

    최종

--- ReservationSystem (1) 2/src/main/java/User_Main/HotelBookingState.java
+++ ReservationSystem (1) 2/src/main/java/User_Main/HotelBookingState.java
@@ -4,7 +4,6 @@ import javax.swing.ButtonGroup;
 import javax.swing.JPanel;
 
 // HotelBookingState 클래스 정의 - FormState 인터페이스 구현
-
 public class HotelBookingState implements FormState {
 
     @Override
@@ -14,6 +13,9 @@ public class HotelBookingState implements FormState {
         ButtonGroup roomTypeGroup = new ButtonGroup();
         ButtonGroup tripTypeGroup = new ButtonGroup();
         ButtonGroup insuranceGroup = new ButtonGroup();
+        ButtonGroup hotelRegistrationGroup = new ButtonGroup();
+        ButtonGroup airlineRegistrationGroup = new ButtonGroup();
+        ButtonGroup carRegistrationGroup = new ButtonGroup();
 
         JPanel formPanel = builder
                 // 호텔 예약 정보
@@ -36,9 +38,9 @@ public class HotelBookingState implements FormState {
                 .addRadioButton("roomType", "스탠다드룸", 320, 300, roomTypeGroup)
                 .addLabel("숙박 비용:", 20, 340, 100, 25)
                 .addTextField("cost", 140, 340, 200, 25)
-                .addLabel("등록 여부:", 20, 380, 100, 25)
-                .addTextField("registration", 140, 380, 200, 25)
-                .addLabel("호텔 대표 사진:", 20, 420, 100, 25)
+                .addLabel("호텔 등록 여부:", 20, 380, 100, 25)
+                .addRadioButton("hotelRegistrationYes", "예", 140, 380, hotelRegistrationGroup)
+                .addRadioButton("hotelRegistrationNo", "아니오", 200, 380, hotelRegistrationGroup)
 
                 // 항공 예약 정보
                 .addLabel("항공 비즈니스 넘버:", 20, 460, 150, 25)
@@ -56,8 +58,9 @@ public class HotelBookingState implements FormState {
                 .addRadioButton("tripType", "편도", 200, 660, tripTypeGroup)
                 .addLabel("항공 비용:", 20, 700, 100, 25)
                 .addTextField("airlineCost", 140, 700, 200, 25)
-                .addLabel("등록 여부:", 20, 740, 100, 25)
-                .addTextField("airlineRegistration", 140, 740, 200, 25)
+                .addLabel("항공 등록 여부:", 20, 740, 100, 25)
+                .addRadioButton("airlineRegistrationYes", "예", 140, 740, airlineRegistrationGroup)
+                .addRadioButton("airlineRegistrationNo", "아니오", 200, 740, airlineRegistrationGroup)
 
                 // 렌터카 예약 정보
                 .addLabel("렌터카 예약 번호:", 20, 780, 150, 25)
@@ -76,7 +79,8 @@ public class HotelBookingState implements FormState {
                 .addRadioButton("insurance", "O", 170, 1020, insuranceGroup)
                 .addRadioButton("insurance", "X", 200, 1020, insuranceGroup)
                 .addLabel("렌터카 등록 여부:", 20, 1060, 150, 25)
-                .addTextField("carRegistration", 170, 1060, 200, 25)
+                .addRadioButton("carRegistrationYes", "예", 170, 1060, carRegistrationGroup)
+                .addRadioButton("carRegistrationNo", "아니오", 230, 1060, carRegistrationGroup)
 
                 .addButton("예약 완료", 290, 1100, 120, 30, e -> context.completeReservation(builder.build())) // 예약 완료 버튼 위치 수정
                 .addButton("닫기", 20, 20, 80, 30, e -> context.goBack()) // 닫기 버튼 위치 수정
